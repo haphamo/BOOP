dec4a975340c475aacefedb85c7ad5be9d69de6f
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-23 12:30:49 -0500

    Removed the get and post routes - Uploadcare

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Widget } from "@uploadcare/react-widget";
+import React, { Fragment, useState, useEffect } from 'react';
+import { Widget } from '@uploadcare/react-widget';
 
-export default function Upload() {
 
+export default function Upload(props) {
+  const [uuid, setUuid] = useState("");
+  const [results, setResults] = useState([]);
+  
   return (
-    <div class="upload">
-      <Widget 
-        publicKey='e409ed1db8c88f8b8083'
-        onChange={console.log} 
-        onFileSelect={console.log}
-        clearable
-        previewStep='true'
-        crop='true'
-      />
-    </div>
+    <Fragment>
+      <div class="upload">
+        <Widget 
+          publicKey='e409ed1db8c88f8b8083'
+          onChange={console.log} 
+          onFileSelect={console.log}
+          clearable
+          previewStep='true'
+          crop='true'
+        />
+      </div>
+    </Fragment>
   )
 }
