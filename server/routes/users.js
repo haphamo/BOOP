83bd9abf62119269e80b0c020d5d938e49370c5b
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-25 16:18:09 -0500

    Added route to get all the pets of a single user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 module.exports = db => {
-  //get all users
+  // Get all users
   router.get("/", (req, res) => {
     // const user = request.session.user_id;
     db.query(`SELECT * FROM users`)
@@ -14,9 +14,35 @@ module.exports = db => {
     })
     .catch(err => {
       res.status(500)
-      res.json({ error: err.message });
+      res.json({ error: err.message })
     })
-  });
+  })
+
+  // Get all the pets of a single user
+  router.get("/pets/:id", (req, res) => {
+    const userId = parseInt(req.params.id)
+    db.query(
+      `SELECT users.first_name AS owner,
+              pets.name AS pet,
+              users.city AS home,
+              users.profile_photo AS user_avatar,
+              pets.profile_photo AS pet_avatar
+      FROM users
+      JOIN pets ON owner_id = users.id
+      WHERE users.id = $1`
+      , [userId])
+    .then(result => {
+      res.json({
+        status: 'Success',
+        result: result.rows,
+        message: 'Retrieved all the pets of a single user'
+      })
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ error: err.message })
+    })
+  })
 
   return router;
 };
