c419b672e52dbb1b080deacd273b2cc90e5aa62d
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-29 17:14:34 -0500

    Made the pet profiles on the Friends page linkable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 
 import './App.scss';
@@ -221,7 +222,8 @@ function Friends(props) {
         <div className={classes.root}>
           <h4>{friend.owner} and {friend.pet}</h4>
             <Avatar alt={friend.owner} src={friend.owner_photo} className={classes.bigAvatar} />
-            <Avatar alt={friend.pet} src={friend.pet_photo} className={classes.bigAvatar} />
+            {/* <Avatar alt={friend.pet} src={friend.pet_photo} className={classes.bigAvatar} /> */}
+            <Link to={`/pets/${friend.pet_id}`} ><Avatar alt={friend.pet} src={friend.pet_photo} className={classes.bigAvatar} /></Link>
         </div>
       </div>
     )
