717014612d4ee2eff6a84361486ec9617b2c7615
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-12-02 17:12:15 -0500

    Still designing the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
-// import { Player } from 'video-react';
+import { Player } from 'video-react';
 // import '~video-react/styles/scss/video-react';
 
 const useStyles = makeStyles(theme => ({
@@ -98,10 +98,10 @@ export default function Homepage (props) {
         : 
       <div>
         <h1>Homepage</h1>
+        
           {/* <Player>
             <source src="" />
           </Player> */}
-        {/* <button onClick={()=> setShowLogin(true)}>Login</button> */}
         <Button variant="contained" 
             color="primary" 
             className={classes.button}
