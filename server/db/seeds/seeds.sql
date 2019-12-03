5c92c0f7dca9670b4e24bcd75354da7e2bb002f8
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-12-03 11:28:27 -0500

    Added new seed data and removed comments

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -5,7 +5,10 @@ VALUES ('Maria', 'Fern', 'mariafern@gmail.com', 'maria', 'Toronto', 'M6G3X4', 'h
 ('Barack', 'Obama', 'bobama@gmail.com', 'barack', 'Toronto', 'M6G1J5', 'https://image.flaticon.com/icons/svg/920/920955.svg'),
 ('Leslie', 'Mosier', 'lmosier@gmail.com', 'leslie', 'Toronto', 'M5V1M7', 'https://image.flaticon.com/icons/svg/920/920963.svg'),
 ('Tina', 'Kim', 'tkim@gmail.com', 'tina', 'Toronto', 'M5V1L5', 'https://image.flaticon.com/icons/svg/920/920963.svg'),
-('Shirley', 'Braha', 'sbraha@gmail.com', 'shirley', 'Toronto', 'M5V1L3', 'https://image.flaticon.com/icons/svg/920/920963.svg');
+('Shirley', 'Braha', 'sbraha@gmail.com', 'shirley', 'Toronto', 'M5V1L3', 'https://image.flaticon.com/icons/svg/920/920963.svg'),
+('Louisa', 'Smith', 'lsmith@gmail.com', 'louisa', 'Toronto', 'M5V1L6', 'https://image.flaticon.com/icons/svg/920/920963.svg'),
+('Tammie', 'Chung', 'tchung@gmail.com', 'tammie', 'Toronto', 'M5V1L7', 'https://image.flaticon.com/icons/svg/920/920963.svg'),
+('BIG COHORT', '2019', 'bigcohort19@gmail.com', 'synergy', 'Toronto', 'M5V1L7', 'https://www.ntaskmanager.com/wp-content/uploads/2019/07/diversified-culture-is-sign-of-good-teamwork.-1.jpg');
 
 INSERT INTO pets (name, age, breed, quirky_fact, owner_id, profile_photo)
 VALUES ('Yoyo', 5, 'Maltese', 'I do not like belly rubs.', 1, 'https://www.rover.com/blog/wp-content/uploads/2018/07/animal-753418_1280-960x540.jpg'),
@@ -16,7 +19,9 @@ VALUES ('Yoyo', 5, 'Maltese', 'I do not like belly rubs.', 1, 'https://www.rover
 ('Sunny', 7, 'Portuguese Water Dog', 'I am the younger sister of Bo.', 4, 'https://dcist.com/wp-content/uploads/sites/3/2013/09/sunnyobama-thumb-640xauto-807922-600x400.jpg'),
 ('Doug the Pug', 7, 'Pug', 'I have appeared in several music videos.', 5, 'https://images-na.ssl-images-amazon.com/images/I/51or1XtdsYL._SX258_BO1,204,203,200_.jpg'),
 ('Winston', 7, 'Corgi', 'I am the unicorn of corgis.', 6, 'http://www.puchic.com/pics/winston-corgi.jpg'),
-('Marnie', 18, 'Shih Tzu', 'I have my own book.', 7, 'https://shortyawards.imgix.net/entries/8th/screen-shot-2016-01-19-at-125145-a.png?auto=format&fit=crop&h=400&q=65&w=400&s=b26d616ab5a53478b99509be499d3838');
+('Marnie', 18, 'Shih Tzu', 'I have my own book.', 7, 'https://shortyawards.imgix.net/entries/8th/screen-shot-2016-01-19-at-125145-a.png?auto=format&fit=crop&h=400&q=65&w=400&s=b26d616ab5a53478b99509be499d3838'),
+('Moo', 10, 'Rescue', 'I may have a dozen toys lying around the house but receipts I find on the floor have got to be my favourite.', 8, 'https://files.slack.com/files-pri/T2G8TE2E5-FQYEU2SDN/snapchat-1162879987.jpg'),
+('Cava', 6, 'Medium Hair Domestic', 'I love yoga and the rain.', 9, 'https://files.slack.com/files-pri/T2G8TE2E5-FQQKRNNDP/snapchat-1669074930.jpg');
 
 INSERT INTO pet_favourites (name, category, pet_id)
 VALUES ('Dentastix', 'Treat', 1),
@@ -43,27 +48,3 @@ VALUES (1, 2, 'ACCEPTED'),
 (2, 3, 'PENDING'),
 (2, 4, 'ACCEPTED'),
 (1, 7, 'ACCEPTED');
-
--- SENDER
--- SELECT users.first_name as sender_name, friends.first_name as receiver_name
--- FROM users
--- JOIN connections ON connections.sender_id = users.id 
--- JOIN users AS friends ON receiver_id = friends.id
--- WHERE users.id = 1;
-
--- SELECT users.first_name as sender_name, friends.first_name as sender_name
--- FROM users
--- JOIN connections ON connections.receiver_id = users.id 
--- JOIN users AS friends ON sender_id = friends.id
--- WHERE users.id = 1;
-
-------- Get all users excluding connections
--- SELECT users.first_name as sender_name, friends.first_name as receiver_name
--- FROM users
--- JOIN connections ON connections.sender_id = users.id 
--- JOIN users AS friends ON receiver_id = friends.id
--- WHERE users.id = 1;
-
--- users.first_name as sender_name, friends.first_name as receiver_name from users join connections ON connections.sender_id = users.id join users as friends on receiver_id = friends.id where users.id = 1
-
-
