78e1be3cc087eedee77b5cf89e9cfb7861677564
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-12-03 01:19:29 -0500

    Styling homepage - checkpoint

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -22,6 +22,7 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    fontWeight: 'bold',
     border: 0,
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
