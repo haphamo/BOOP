6a2034696c22ee0aa10e8a379e656344972922d7
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-02-10 23:10:30 -0500

    Deploy - attempt 3

--- server/index.js
+++ server/index.js
@@ -41,10 +41,10 @@ const petsRoutes = require("./routes/pets");
 
 // DEPLOYING BOOP - TESTING
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'src/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + '/src/build/index.html'))
 })
 
 app.use(cookieSession({
