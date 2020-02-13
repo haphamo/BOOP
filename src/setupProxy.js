e7d0d41fe965158475ab8886183e4d7d064e96e8
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-02-13 18:03:03 -0500

    Removed a / from target

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy('/api/*', { target: 'https://final-boop.herokuapp.com/' }));
+  app.use(proxy('/api/*', { target: 'https://final-boop.herokuapp.com' }));
 };
