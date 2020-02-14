5942a6ea13b58b0c11fc166f668161dbd7fe9abc
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-02-13 19:39:56 -0500

    Removed one wildcard, secure and changeOrigin variables

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy('/api/**', { target: 'http://[::1]:3001', secure: false,
-  changeOrigin: true }));
+  app.use(proxy('/api/*', { target: 'http://[::1]:3001' }));
 };
