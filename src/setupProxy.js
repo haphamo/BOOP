84fe2c6c4548c1c55e5478d3a4c7a1b5ca50ac05
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-02-13 19:14:30 -0500

    Added another * to proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy('/api/', { target: 'http://localhost:3001', secure: false,
+  app.use(proxy('/api/**', { target: 'http://localhost:3001', secure: false,
   changeOrigin: true }));
 };
