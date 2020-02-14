c35b8718e7608389067671ad3cbf0bf40bceb21b
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-02-13 19:07:16 -0500

    Added secure and changeOrigin variables again to setupProxy file

--- server/index.js
+++ server/index.js
@@ -35,7 +35,6 @@ const petsRoutes = require("./routes/pets");
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
