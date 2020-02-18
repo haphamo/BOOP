0f8aa73d9d360fb610bec5e27af0c09659483c3e
haphamo <hapham0392@gmail.com>
2020-02-17 22:51:57 -0500

    static front end renders from prev commit, changed cors back to prev state and removed the app use *

--- server/index.js
+++ server/index.js
@@ -46,22 +46,13 @@ app.use(passport.initialize());
 // Deserialize cookie from the browser
 app.use(passport.session());
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, '/build', 'index.html'));
-})
 // Use CORS and File Upload modules here
 
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(cors({
+  origin: "http://localhost:3000/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204
+}))
 
 // Register, Login and Logout Routes
 // Create a new user
